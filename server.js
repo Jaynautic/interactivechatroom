7fd3a44155934be075dba21d17dd426fdb64cc53
Jaynautic <jaynautic1@gmail.com>
2025-06-05 01:38:51 -0400

    fixed server

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ server.on('connection', (socket) => {
   clients.push(socket);
 
   socket.on('message', (msg) => {
+    const json = msg.toString()
+    const parsed = JSON.parse(json)
+    console.log(parsed.text)
+    console.log(msg.toString())
     for (let c of clients) {
       if (c !== socket && c.readyState === WebSocket.OPEN) {
-        c.send(msg);
+        c.send(json);
       }
     }
   });
